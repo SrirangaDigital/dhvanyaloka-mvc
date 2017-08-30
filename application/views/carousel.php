cc93c1f1d543c8c3f15802ef54d3a4d504632a5c
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-08-30 21:12:21 +0530

    carousel updated

--- application/views/carousel.php
+++ application/views/carousel.php
@@ -11,16 +11,16 @@
                 <div class="carousel-inner">
                     <div class="item active">
                         <img src="<?=PUBLIC_URL?>images/stock/trees.jpg" alt="Dhvanyaloka">
-                        <div class="override-carousel-caption pos-2-3-left scheme-dark">
+                        <div class="override-carousel-caption pos-c-full scheme-dark">
                             <div class="caption-content">
-                                <h3 class="caption-title">Dhvanyaloka</h3>
-                                <p>The Centre is named after Dhvanyaloka, the title of a critical work of the 9th century A.D. by Anandavardhana representing the highest peak of Indian genius in critical response to works of art. “Dhvani” literally means sound</p>
+                                <h3 class="caption-title">Dhvanyaloka Centre for Indian Studies</h3>
+                                <p>A subsidiary of Dhvanyaloka Acadmey Foundation Private Limited</p>
                             </div>
                         </div>
                     </div>
                     <div class="item">
                         <img src="<?=PUBLIC_URL?>images/stock/awards.jpg" alt="Awards">
-                        <div class="override-carousel-caption pos-2-3-left scheme-dark">
+                        <div class="override-carousel-caption pos-c-full scheme-dark">
                             <div class="caption-content">
                                 <h3 class="caption-title">Dhvanyaloka</h3>
                                 <p>The Centre is dedicated to the renewal of this rich potential in our approach to work of art. Svalpam Apyasya Dharmasya!</p>

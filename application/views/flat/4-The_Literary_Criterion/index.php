76788260b220143ff92a4a9641047c15be3dd6d1
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-08-30 20:00:03 +0530

    publications - literary criterion and sarasa included

---
+++ application/views/flat/4-The_Literary_Criterion/index.php
@@ -0,0 +1,34 @@
+<div class="col-md-3 clear-paddings subnav">
+    <ul class="">
+        <li><a href="Special_Issues">Special Issues</a></li>
+        <li><a href="#Subscription">Subscription</a></li>
+    </ul>
+</div>
+<div class="col-md-9 clear-paddings">
+    <!-- Breadcrumb will be inserted here -->
+    <?=$this->printBreadcrumb($path)?>
+
+    <h1>The Literary Criterion</h1>
+    <h2>Founded in 1952 by Professor C.D.Narasimhaiah</h2>
+
+    <h4>Editor:</h4>
+    <p>C.N.Srinath</p>
+
+	<h4>Assc. Editor:</h4>
+	<p>Ragini Ramachandra</p>
+
+	<p>The Literary Criterion  the most influential critical journal in English in India for the past 58 years, apart from generating a critical climate, has introduced American literature, Commonwealth Literature, and promoted Indian writing in English Translation studies and our own ancient traditional critical theories.  Its singular objective is to win attention to deserving literary texts especially by making an Indian approach thereby fusing the classical with the contemporary.</p>
+	<p>It has also brought out more than twenty five special issues some of which are still available for sale.</p>
+
+	<h3 id="Subscription">Subscription</h3>
+	<p>Annual subscription for the quarterly is – individuals : Rs.500/-, institutions: Rs.800/-; Foreign for both individuals and institutions - $30 + $10 for airmail postage.  Some back numbers are available for sale.</p>
+	<p>
+		All correspondence regarding contribution and subscription with the Editor,<br />
+		The Literary Criterion,<br />
+		Dhvanyaloka, Bogadi Road,<br />
+		Mysore 560 006, INDIA<br />
+		Email: srinathcn43@gmail.com<br />
+		Mobile : +91 740 636 3248
+	</p>
+
+</div>
---
+++ application/views/flat/5-Sarasa/index.php
@@ -0,0 +1,28 @@
+<div class="col-md-3 clear-paddings subnav">
+    <ul class="">
+        <li><a href="#Subscription">Subscription</a></li>
+    </ul>
+</div>
+<div class="col-md-9 clear-paddings">
+    <!-- Breadcrumb will be inserted here -->
+    <?=$this->printBreadcrumb($path)?>
+
+    <h1>Sarasa</h1>
+    <h2>Founded by CN Srinath in 2000</h2>
+
+    <h4>Editor:</h4>
+    <p>C.N.Srinath</p>
+
+    <p>Sarasa founded by CN Srinath in 2000 is probably the only journal in India devoted exclusively to creative translation from Indian languages into English. It has so for published various writers, both classical and contemporary from Punjabi, Oriya, Kannada, Hindi, Tamil, Urdu, Telugu, Sanskrit and other languages with a focus on younger contemporary writers of both promise and achievement.</p>
+	<p>It is good to remind ourselves that no theory has generated creativity though it may have helped us in organizing our perception of it.  Hence the focus will largely be on actual translation.  But there will be a discussion of actual professional/practitioner’s specific problems, for example translation of cultural words and phrases from our languages into English, expressions that have a local scent and flavour, a halo of historical past, the heft and load of layers of meaning embedded into it.</p>
+	<p>It is only the specific that can contribute something to the general.</p>
+
+	<h3 id="Subscription">Subscription</h3>
+	<p>Annual subscription for the Bi-annual is - individuals and institutions: Rs.300/-, foreign $15.00 (airmail  postage free). Contributions are invited.</p>
+	<p>
+		All correspondence regarding contribution and subscription with the Editor,<br />
+		Sarasa, Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA<br />
+		Email: srinathcn43@gmail.com<br />
+		Mobile : +91 740 636 3248
+	</p>
+</div>

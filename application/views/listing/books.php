ebf662348627f1bd35af3049a966af0ddabb2a58
shivasdst <shivably04sdst@gmail.com>
2017-09-06 13:52:20 +0530

    updated books view

--- application/views/listing/books.php
+++ application/views/listing/books.php
@@ -6,7 +6,7 @@
                 <div class="flat-page">
                     <div class="col-md-3 clear-paddings subnav">
                         <ul class="">
-                            <li><a class="active" href="Special_Issues">Special Issues</a></li>
+                            <li><a class="active" href="<?=BASE_URL?>listing/books">Special Issues</a></li>
                             <li><a href="../The_Literary_Criterion/#Subscription">Subscription</a></li>
                         </ul>
                     </div>
@@ -22,15 +22,7 @@
                         <h2>The Literary Criterion</h2>
 
                         <p>The Literary Criterion, the most influential journal as we all know, has made significant contribution to the growth of English studies in India. It has consistently promoted Indian writing in English and poetics, American Literature, Commonwealth Literature, Translation and the study of Regional literatures.</p>
-                        <p>The special issues that figures here are 29 in number out of more than 35 produced in print. A sample of 3 such special issues, first three in order,</p>
-                        <ol>
-                            <li>F.R. Leavis Birth Centenary Essays</li>
-                            <li>Problems Of Translation</li>
-                            <li>Nobel Laureates In Literature</li>
-                        </ol>
-
-                        <p>are accessible for free view. If the subscribers are interested in other special issues, the may contact the editor to send the same by DD at Rs. 300/- each. The contents of all special issues are accessible.</p>
-
+                        
                         <div class="cover-image-carousel">
 
 <?php foreach($data as $row) { ?>
@@ -40,6 +32,15 @@
                             </a>
 <?php } ?>
                         </div>
+                        <p>The special issues that figures here are 29 in number out of more than 35 produced in print. A sample of 3 such special issues, first three in order,</p>
+                        <ol>
+                            <li>F.R. Leavis Birth Centenary Essays</li>
+                            <li>Problems Of Translation</li>
+                            <li>Nobel Laureates In Literature</li>
+                        </ol>
+
+                        <p>are accessible for free view. If the subscribers are interested in other special issues, the may contact the editor to send the same by DD at Rs. 300/- each. The contents of all special issues are accessible.</p>
+
                     </div>                       
                 </div>
             </div>

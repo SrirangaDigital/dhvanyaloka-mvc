2bd6b96de694cf43f28107ec14e1e9c82cd09718
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-10-19 14:48:12 +0530

    text conv in progress

--- application/controllers/data.php
+++ application/controllers/data.php
@@ -2,6 +2,7 @@
 
 class data extends Controller {
 
+	public $i = 0;
 	public function __construct() {
 		
 		parent::__construct();
@@ -36,6 +37,76 @@ class data extends Controller {
 
 			$this->view('error/blah');
 		}
+	}
+
+	public function parseHTML() {
+
+
+		$fileName = PUBLIC_URL . 'html/test.html';
+		$xml = simplexml_load_file($fileName);
+
+		$dom = dom_import_simplexml($xml);
+
+		$counter = 1;
+		$xpath = new DOMXpath($dom->ownerDocument);
+		foreach($xpath->query('//text()') as $text_node) {
+
+		    if(preg_replace('/\s+/', '', $text_node->nodeValue)) $text_node->nodeValue = $this->APS2Unicode($text_node->nodeValue);
+		}
+
+		// $this->recurseXML($xml);
+
+		var_dump($xml->asXML());
+	}
+
+	public function recurseXML($xml) {
+
+		$childCount = $xml->count();
+		$ifStringExists = preg_replace('/\s+/', '', $xml->__toString());
+
+		if($childCount > 0) {
+
+			if($ifStringExists) {
+
+				// Compound: Both text and elements are present
+				
+				$dom = dom_import_simplexml($xml);
+
+				$counter = 1;
+				$xpath = new DOMXpath($dom->ownerDocument);
+				foreach($xpath->query('//text()') as $text_node) {
+
+					// var_dump($text_node);
+				    $text_node->nodeValue = $this->APS2Unicode($text_node->nodeValue);
+				}
+
+				// echo $sx->asXML();
+
+				foreach ($xml as $key => $child) {
+
+					$this->recurseXML($child);
+				}
+				// $xml[0] = $this->APS2Unicode($xml->__toString());
+			}	
+			else{
+
+				// Recursive: only elements found
+				foreach ($xml as $key => $child) {
+
+					$this->recurseXML($child);
+				}
+			}
+		}
+		else {
+
+			// Only text found
+			$xml[0] = $this->APS2Unicode($xml->__toString());
+		}
+	}
+
+	public function APS2Unicode($text) {
+
+		return 'Hello';
 	}
 }
 
